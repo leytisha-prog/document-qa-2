2b31724153f720ca6a0d665bffcb1ed910e2966c
leytisha-prog <lejack@syr.edu>
2026-01-20 21:41:32 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,28 +45,12 @@ else:
         uploaded_file = st.file_uploader(
             "Upload a document (.txt or .pdf)", type=("txt", "pdf")
         )
-     
-    except Exception:
-        st.error("Invalid API key. Please try again.", icon="❌")
-
-
-    
-    # Existing code -------
-    # Create an OpenAI client.
-    #client = OpenAI(api_key=openai_api_key)
-
-    # Let the user upload a file via `st.file_uploader`.
-    #uploaded_file = st.file_uploader(
-        #"Upload a document (.txt or .pdf)", type=("txt", "pdf")
-    #)
-
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
+        question = st.text_area(
         "Now ask a question about the document!",
         placeholder="Can you give me a short summary?",
         disabled=not uploaded_file,
     )
-    if uploaded_file and question:
+        if uploaded_file and question:
             
             # Get the file type
             file_extension = uploaded_file.name.split(".") [-1]
@@ -98,3 +82,19 @@ else:
 
      # Stream the response to the app using `st.write_stream`.
             st.write_stream(stream)
+    except Exception:
+        st.error("Invalid API key. Please try again.", icon="❌")
+
+
+    
+    # Existing code -------
+    # Create an OpenAI client.
+    #client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via `st.file_uploader`.
+    #uploaded_file = st.file_uploader(
+        #"Upload a document (.txt or .pdf)", type=("txt", "pdf")
+    #)
+
+    # Ask the user for a question via `st.text_area`.
+    
